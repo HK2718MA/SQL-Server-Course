e2deba7303cf4424eb607ecfd0cca41cd7c19f0b
HK2718MA <hk188789@gmail.com>
2025-08-03 23:02:41 +0300

    c# 1

---
+++ Program1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello!");
+        Console.WriteLine("Input the first number:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Input the second number:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("What do you want to do with those numbers?");
+        Console.WriteLine("[A]dd");
+        Console.WriteLine("[S]ubtract");
+        Console.WriteLine("[M]ultiply");
+
+        string choice = Console.ReadLine().ToLower();
+        switch (choice)
+        {
+            case "a":
+                Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                break;
+
+            case "s":
+                Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                break;
+
+            case "m":
+                Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                break;
+
+            default:
+                Console.WriteLine("Invalid option");
+                break;
+        }
+        Console.WriteLine("Press any key to close");
+        Console.ReadKey();
+    }
+}
